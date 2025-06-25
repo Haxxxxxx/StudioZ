a2113e4792339ee5b68833c17dab26827dc667fd
Noham Coulon <noham.coulon@gmail.com>
2025-06-25 11:55:19 +0200

    [ADD] Current level string to networkPlayerData

--- StudioZCore/Assets/Scripts/Networking/NetworkPlayerData.cs
+++ StudioZCore/Assets/Scripts/Networking/NetworkPlayerData.cs
@@ -12,10 +12,12 @@ public class NetworkPlayerData : NetworkBehaviour
 {
     private readonly SyncVar<int> _score = new SyncVar<int>();
     private readonly SyncVar<string> _name = new SyncVar<string>();
+    private readonly SyncVar<string> _level = new SyncVar<string>();
     private readonly SyncVar<string> _avatarID = new SyncVar<string>();
     
     [SerializeField] private TMP_Text scoreText;
     [SerializeField] private TMP_Text playerNameText;
+    [SerializeField] private TMP_Text currentLvlText;
     [SerializeField] private Image img;
 
     public override void OnStartClient()
@@ -27,6 +29,7 @@ public class NetworkPlayerData : NetworkBehaviour
         //SetScore(GameManager.Instance.GetScore()); 
         SetName(SaveManager.Instance.playerData.playerName);
         SetAvatar(SaveManager.Instance.playerData.selectedAvatarName);
+        SetLevel("Not in a level");
         
         // Subscribe to score changes
         //GameManager.OnScoreChanged += OnScoreChanged;
@@ -41,18 +44,24 @@ public class NetworkPlayerData : NetworkBehaviour
     {
         _score.Value = value;
         scoreText.text = "Score: " + value;
-        Debug.LogWarning("SetScore RPC called with value: " + value);
+        //Debug.LogWarning("SetScore RPC called with value: " + value);
     }
     [ServerRpc(RunLocally = true)] private void SetName(string value)
     {
         _name.Value = value;
         playerNameText.text = value;
-        Debug.LogWarning("SetName RPC called with value: " + value);
+       //Debug.LogWarning("SetName RPC called with value: " + value);
+    }
+    [ServerRpc(RunLocally = true)] private void SetLevel(string value)
+    {
+        _level.Value = value;
+        currentLvlText.text = value;
+        //Debug.LogWarning("SetLevel RPC called with value: " + value);
     }
     [ServerRpc(RunLocally = true)] private void SetAvatar(string id)
     {
         _avatarID.Value = id;
         img.sprite = Resources.Load<Sprite>("Avatars/" + id);
-        Debug.LogWarning("SetName RPC called with value: " + id);
+        //Debug.LogWarning("SetName RPC called with value: " + id);
     }
 }
