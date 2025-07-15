77206d79d442faa7fb4d15a45b050b47a0010269
Noham Coulon <44196664+coucoul38@users.noreply.github.com>
2025-07-15 12:22:03 +0200

    WIP

--- StudioZCore/Assets/Scripts/MiniGames/Episode1/6-Shopping/MG6_Bundle.cs
+++ StudioZCore/Assets/Scripts/MiniGames/Episode1/6-Shopping/MG6_Bundle.cs
@@ -5,9 +5,16 @@ using UnityEngine;
 public class MG6_Bundle : MonoBehaviour
 {
     [SerializeField] private bool viable = false;
+    [SerializeField] private int price = 6;
 
+    
     public bool IsViable()
     {
         return viable;
+    }
+
+    public int GetPrice()
+    {
+        return price;
     }
 }
--- StudioZCore/Assets/Scripts/MiniGames/Episode1/6-Shopping/MG6_Shopping.cs
+++ StudioZCore/Assets/Scripts/MiniGames/Episode1/6-Shopping/MG6_Shopping.cs
@@ -1,7 +1,34 @@
 using System.Collections.Generic;
+using UnityEditor.AddressableAssets.Build.Layout;
 using UnityEngine;
 
 public class MG6_Shopping : MiniGameBase
 {
-    [SerializeField] private List<List<DraggableItem>> BundleCombinations;
+    [SerializeField] private List<List<DraggableItem>> bundleCombinations;
+
+    [SerializeField] private int coins;
+    
+    public void SelectBundle(MG6_Bundle bundle)
+    {
+        // Remove coins
+        if (bundle.GetPrice() > coins)
+        {
+            return;
+        }
+        
+        coins -= bundle.GetPrice();
+        
+        if (bundle.IsViable())
+        {
+            // add score
+            currentScore += 2;
+        }
+        else
+        {
+            // remove score
+            currentScore -= 1;
+        }
+        
+        // Next bundle proposition
+    }
 }
