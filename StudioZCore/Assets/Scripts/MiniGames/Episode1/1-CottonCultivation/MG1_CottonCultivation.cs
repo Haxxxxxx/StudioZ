a0c89ab9063a177c619c6426be02b87fbc8cdd7f
Evan Choquet <echoquet@gaming.tech>
2025-07-21 10:09:14 +0200

    ADD minigame namespace

---
+++ StudioZCore/Assets/Scripts/MiniGames/Episode1/1-CottonCultivation/MG1_CottonCultivation.cs
@@ -0,0 +1,258 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine.UI;
+using System;
+using TMPro;
+using Coffee.UIEffects;
+
+namespace MiniGames
+{
+    namespace Episode1
+    {
+        public class MG1_CottonCultivation : MiniGameBase
+        {
+
+            #region Variables
+
+            public static MG1_CottonCultivation instance { get; private set; }
+
+            public class CottonCultivationActionName : MiniGameActionName
+            {
+                public string PickHealthySeed { get; private set; } = "pick_healthy_seed";
+                public string PickCorruptedSeed { get; private set; } = "pick_corrupted_seed";
+                public string ThrowHealthySeed { get; private set; } = "throw_healthy_seed";
+                public string ThrowCorruptedSeed { get; private set; } = "throw_corrupted_seed";
+                public string UseShovel { get; private set; } = "use_shovel";
+                public string UseShovelIncorrectly { get; private set; } = "use_shovel_incorrectly";
+                public string UseWateringCan { get; private set; } = "use_watering_can";
+                public string UseSunlight { get; private set; } = "use_sunlight";
+                public string UseGlove { get; private set; } = "use_glove";
+            }
+
+            public enum ToolsType
+            {
+                None,
+                Shovel,
+                WateringCan,
+                Sunlight,
+                Glove,
+            }
+
+            [Header("MiniGame Settings")]
+            private ToolsType toolType = ToolsType.None;
+            [SerializeField] private int cottonToHarvest = 1;
+            private int cottonHarvested = 0;
+            public CottonCultivationActionName miniGameActionName = new CottonCultivationActionName();
+
+            [Header("UI References")]
+            [SerializeField] private List<Button> toolsBtn = new List<Button>();
+            [SerializeField] private TextMeshProUGUI cottonText;
+
+            private Button selectedToolBtn;
+
+            [field: NonSerialized] public List<FieldHole_CottonCultivation> fieldHoles { get; private set; } = new List<FieldHole_CottonCultivation>();
+
+            private List<Seed_CottonCultivation> unsortedSeed = new List<Seed_CottonCultivation>();
+            private List<Seed_CottonCultivation> goodSortedSeed = new List<Seed_CottonCultivation>();
+            private List<Seed_CottonCultivation> badSortedSeed = new List<Seed_CottonCultivation>();
+            private float minDistance;
+
+            #endregion
+
+
+            protected override void Awake()
+            {
+                instance = this;
+
+                base.Awake();
+
+                fieldHoles = FindObjectsByType<FieldHole_CottonCultivation>(FindObjectsSortMode.None).ToList<FieldHole_CottonCultivation>();
+                unsortedSeed = FindObjectsByType<Seed_CottonCultivation>(FindObjectsSortMode.None).ToList<Seed_CottonCultivation>();
+            }
+
+
+            public override void StartGame()
+            {
+                Debug.Log("Cotton Cultivation MiniGame Started");
+                base.StartGame();
+            }
+
+            #region Phase1
+
+            public void UpdateUnsortedSeed(Seed_CottonCultivation sortedSeed, bool goodSeed, GameObject container)
+            {
+                if (!unsortedSeed.Remove(sortedSeed))
+                {
+                    Debug.LogWarning($"Attempted to sort a seed that is not in the unsorted list: {sortedSeed.name}");
+                    return;
+                }
+
+                /*   Vector2 itemSize = sortedSeed.GetComponent<RectTransform>().rect.size * sortedSeed.GetComponent<RectTransform>().lossyScale;
+                   minDistance = Mathf.Max(itemSize.x, itemSize.y);
+
+                   float radius = Mathf.Min(container.GetComponent<RectTransform>().rect.width, container.GetComponent<RectTransform>().rect.height) / 2f ;
+                   Vector2 pos;
+                   int attempts = 0;
+                   const int maxAttempts = 50;
+
+                   do
+                   {
+                       pos = UnityEngine.Random.insideUnitCircle * radius;
+                       sortedSeed.GetComponent<RectTransform>().anchoredPosition = pos;
+                       attempts++;
+                       if (attempts > maxAttempts) break;
+                   } while (IsOverlapping(pos, (goodSeed ? goodSortedSeed : badSortedSeed)));
+
+                   sortedSeed.transform.SetParent(container.transform, false);
+                   sortedSeed.GetComponent<RectTransform>().anchoredPosition = pos;*/
+
+                (goodSeed ? goodSortedSeed : badSortedSeed).Add(sortedSeed);
+
+                if (unsortedSeed.Count == 0)
+                {
+                    GameObject seeds = GameObject.Find("Seeds");
+                    goodSortedSeed.ForEach(seed =>
+                    {
+                        seed.draggableItem.enabled = true;
+                        seed.transform.SetParent(seeds.transform);
+                    });
+                }
+            }
+
+            private bool IsOverlapping(Vector2 newPos, List<Seed_CottonCultivation> items)
+            {
+                foreach (var item in items)
+                {
+                    if (Vector2.Distance(item.GetComponent<RectTransform>().anchoredPosition, newPos) < minDistance)
+                        return true;
+                }
+                return false;
+            }
+
+            #endregion
+
+
+            #region Phase2
+
+            public void CheckToolTypeForHole(FieldHole_CottonCultivation currentHole)
+            {
+                switch (toolType)
+                {
+                    case ToolsType.None:
+                        break;
+                    case ToolsType.Shovel:
+                        UseShovel(currentHole);
+                        break;
+                    case ToolsType.WateringCan:
+                        UseWateringCan(currentHole);
+                        break;
+                    case ToolsType.Sunlight:
+                        UseSunlight(currentHole);
+                        break;
+                    case ToolsType.Glove:
+                        UseGlove(currentHole);
+                        break;
+                    default:
+                        Debug.LogWarning("No tool selected or tool not applicable for the current hole state.");
+                        break;
+                }
+            }
+
+            #region Tools 
+
+            private void UseShovel(FieldHole_CottonCultivation currentHole)
+            {
+                if (currentHole.holeState == FieldHole_CottonCultivation.HoleState.Empty)
+                {
+                    currentHole.SetHoleState(FieldHole_CottonCultivation.HoleState.HoleCreated);
+                    PerformAction(miniGameActionName.UseShovel);
+                }
+                else if (currentHole.holeState == FieldHole_CottonCultivation.HoleState.Seeded)
+                {
+                    currentHole.SetHoleState(FieldHole_CottonCultivation.HoleState.HoleFilled);
+                    PerformAction(miniGameActionName.UseShovel);
+                }
+                else if (currentHole.holeState != FieldHole_CottonCultivation.HoleState.HoleCreated && currentHole.holeState != FieldHole_CottonCultivation.HoleState.HoleFilled)
+                {
+                    PerformAction(miniGameActionName.UseShovelIncorrectly);
+                }
+            }
+
+            private void UseWateringCan(FieldHole_CottonCultivation currentHole)
+            {
+                if (currentHole.holeState == FieldHole_CottonCultivation.HoleState.HoleFilled)
+                {
+                    currentHole.SetHoleState(FieldHole_CottonCultivation.HoleState.Watered);
+                }
+                else if (currentHole.holeState != FieldHole_CottonCultivation.HoleState.Watered)
+                {
+                    //? Perform negativeAction for using watering can in bad state
+                }
+            }
+
+            private void UseSunlight(FieldHole_CottonCultivation currentHole)
+            {
+                if (currentHole.holeState == FieldHole_CottonCultivation.HoleState.Watered)
+                {
+                    currentHole.SetHoleState(FieldHole_CottonCultivation.HoleState.Sunny);
+                }
+                else if (currentHole.holeState != FieldHole_CottonCultivation.HoleState.Sunny)
+                {
+                    //? Perform negativeAction for using sunlight in bad state
+                }
+            }
+
+            private void UseGlove(FieldHole_CottonCultivation currentHole)
+            {
+                if (currentHole.seededSeed.seedState == Seed_CottonCultivation.SeedState.CottonReady)
+                {
+                    cottonHarvested += currentHole.seededSeed.RecoltCotton();
+                    currentHole.enabled = false;
+                    PerformAction(miniGameActionName.UseGlove);
+
+                    if (cottonText != null)
+                    {
+                        cottonText.text = $"Cotton : {cottonHarvested}";
+                    }
+
+                    if (cottonHarvested >= cottonToHarvest)
+                    {
+                        EndGame();
+                    }
+                }
+                else
+                {
+                    //? Perform negativeAction for using glove in bad state
+                }
+            }
+
+            public void BS_SetSelectedTools(int _toolsType)
+            {
+                toolType = (ToolsType)(_toolsType + 1);
+
+                toolsBtn.ForEach(btn =>
+                {
+                    btn.image.color = Color.white;
+                    btn.GetComponentInChildren<UIEffect>().enabled = false;
+                }
+                );
+
+                if (selectedToolBtn == toolsBtn[_toolsType])
+                {
+                    selectedToolBtn = null;
+                    toolType = ToolsType.None;
+                }
+                else
+                {
+                    selectedToolBtn = toolsBtn[_toolsType];
+                    selectedToolBtn.GetComponentInChildren<UIEffect>().enabled = true;
+                }
+            }
+
+            #endregion
+
+            #endregion
+        }
+    }
+}
---
+++ StudioZCore/Assets/Scripts/MiniGames/Episode1/4-PatternWeaving/MG4_PatternWeaving.cs
@@ -0,0 +1,308 @@
+using System.Collections;
+using UnityEngine;
+using System.Collections.Generic;
+using System;
+using UnityEngine.UI;
+
+
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace MiniGames
+{
+    namespace Episode1
+    {
+        [ExecuteAlways]
+        public class MG4_PatternWeaving : MiniGameBase
+        {
+            #region Variables
+
+            public static MG4_PatternWeaving instance { get; private set; }
+
+            public class PatternWeavingActionName : MiniGameActionName
+            {
+                public string GoodWeaving { get; private set; } = "good_weaving";
+                public string BadWeaving { get; private set; } = "bad_weaving";
+            }
+
+            [System.Serializable]
+            public class Pattern
+            {
+                public Texture2D texture;
+                public GridCell_PatternWeaving startCell;
+                [NonSerialized] public bool isFinished = false;
+
+                public Pattern(Texture2D texture, GridCell_PatternWeaving startCell)
+                {
+                    this.texture = texture;
+                    this.startCell = startCell;
+                }
+            }
+
+            [Header("MiniGame Settings")]
+            public PatternWeavingActionName miniGameActionName = new PatternWeavingActionName();
+            [NonSerialized] public GridCell_PatternWeaving nextCell;
+            private bool firstPattern = false;
+
+            [Header("Grid References")]
+            [SerializeField] private RectTransform gridParent;
+            [SerializeField] private GameObject gridPrefab;
+            [SerializeField] private int gridWidth;
+            [SerializeField] private int gridHeight;
+            [SerializeField] private float gridSpacing;
+
+            [Header("Pattern Settings")]
+            [SerializeField] private GameObject patternSelectorCanvas;
+            [SerializeField] private Pattern selectedPattern;
+            [SerializeField][NonReorderable] private List<Pattern> patterns = new List<Pattern>();
+
+            [Header("Editor Settings")]
+            public bool editPatternPath = false;
+            public GridCell_PatternWeaving lastSelectedCell;
+
+            [Header("UI References")]
+            [SerializeField] private Image patternImg;
+
+
+            #endregion
+
+            protected override void Awake()
+            {
+                instance = this;
+
+                base.Awake();
+            }
+
+            protected override void Start()
+            {
+                base.Start();
+
+                ClearGrid();
+                patternImg.gameObject.SetActive(false);
+                patternSelectorCanvas.SetActive(true);
+            }
+
+
+            #region Editor Funcions
+
+#if UNITY_EDITOR
+
+            [ContextMenu("Generate Circular Grid")]
+            public void GenerateCircularGrid()
+            {
+                if (gridPrefab == null || gridParent == null)
+                {
+                    Debug.LogError("Prefab ou Parent non assigné.");
+                    return;
+                }
+
+                for (int i = gridParent.childCount - 1; i >= 0; i--)
+                {
+                    DestroyImmediate(gridParent.GetChild(i).gameObject);
+                }
+
+                Vector2 center = new Vector2(gridWidth / 2f, gridHeight / 2f);
+                float maxRadius = Mathf.Min(gridWidth, gridHeight) / 2f;
+
+                for (int x = 0; x < gridWidth; x++)
+                {
+                    for (int y = 0; y < gridHeight; y++)
+                    {
+                        Vector2 gridPos = new Vector2(x, y);
+                        Vector2 local = gridPos - center;
+
+                        if (local.magnitude <= maxRadius)
+                        {
+                            GameObject go = (GameObject)PrefabUtility.InstantiatePrefab(gridPrefab, gridParent);
+                            RectTransform rt = go.GetComponent<RectTransform>();
+                            rt.anchoredPosition = local * (local.y * gridSpacing < center.y ? gridSpacing - 0.25f : gridSpacing);
+                            rt.name = $"Cell_{x}_{y}";
+                            GridCell_PatternWeaving cell = go.GetComponent<GridCell_PatternWeaving>();
+                            cell.cellPos = new Vector2Int(x, y);
+                        }
+                    }
+                }
+
+                Debug.Log("Grille circulaire générée dans l’éditeur.");
+            }
+
+            [ContextMenu("Generate Motif")]
+            public void GenerateMotif()
+            {
+                foreach (Transform child in gridParent)
+                {
+                    if (child.TryGetComponent<GridCell_PatternWeaving>(out GridCell_PatternWeaving cell))
+                    {
+                        cell.SetColor(Color.white);
+                        GetCellTargetColorFromMotif(cell);
+                    }
+                }
+                ViewMotif();
+            }
+
+            public void GetCellTargetColorFromMotif(GridCell_PatternWeaving cell)
+            {
+                if (selectedPattern == null)
+                {
+                    Debug.LogError("Selected motif not assigned.");
+                }
+                Vector2Int cellPxPos = GetPixelPositionInImage(cell);
+
+                Color targetColor = selectedPattern.texture.GetPixel(cellPxPos.x, cellPxPos.y);
+
+                if (!AreColorsSimilar(targetColor, new Color(0.965f, 0.875f, 0.780f, 1f)))
+                {
+                    cell.AddPatternData(selectedPattern.texture, targetColor);
+                    Undo.RecordObject(cell, "Add data");
+                    EditorUtility.SetDirty(cell);
+                }
+            }
+
+            private Vector2Int GetPixelPositionInImage(GridCell_PatternWeaving cell)
+            {
+                float localPosX = cell.transform.localPosition.x + gridParent.rect.width / 2f;
+                float localPosY = cell.transform.localPosition.y + gridParent.rect.height / 2f;
+
+
+                int pixelX = (int)(localPosX * selectedPattern.texture.width / gridParent.rect.width);
+                int pixelY = (int)(localPosY * selectedPattern.texture.height / gridParent.rect.height);
+
+                return new Vector2Int(pixelX, pixelY);
+            }
+
+            private void OnEnable()
+            {
+                UnityEditor.Selection.selectionChanged += OnSelectionChanged;
+            }
+
+            private void OnDisable()
+            {
+                UnityEditor.Selection.selectionChanged -= OnSelectionChanged;
+            }
+
+            private void OnSelectionChanged()
+            {
+                if (!editPatternPath || Selection.activeGameObject == null
+                    || !Selection.activeGameObject.TryGetComponent<GridCell_PatternWeaving>(out GridCell_PatternWeaving cell))
+                {
+                    editPatternPath = false;
+                    lastSelectedCell = null;
+                    return;
+                }
+
+                if (lastSelectedCell != null)
+                {
+                    if (lastSelectedCell.data.Find(d => d.texture == selectedPattern.texture) != null)
+                    {
+                        lastSelectedCell.data.Find(d => d.texture == selectedPattern.texture).nextCell = cell;
+                        Undo.RecordObject(lastSelectedCell, "Update Motif Path");
+                        EditorUtility.SetDirty(lastSelectedCell);
+                    }
+                    else
+                    {
+                        Debug.LogError("Last selected cell does not contain the selected motif data.");
+                    }
+                }
+                lastSelectedCell = cell;
+            }
+
+            [ContextMenu("View Motif")]
+            public void ViewMotif()
+            {
+                foreach (Transform child in gridParent)
+                {
+                    if (child.TryGetComponent<GridCell_PatternWeaving>(out GridCell_PatternWeaving cell))
+                    {
+                        if (cell.data.Count != 0 && cell.data.Find(d => d.texture == selectedPattern.texture) != null)
+                        {
+                            cell.SetColor(cell.data.Find(d => d.texture == selectedPattern.texture).color);
+                        }
+                        else
+                        {
+                            cell.SetColor(Color.white);
+                        }
+                    }
+                }
+            }
+
+#endif
+
+            #endregion
+
+            public bool AreColorsSimilar(Color a, Color b, float epsilon = 0.01f)
+            {
+                return Mathf.Abs(a.r - b.r) < epsilon &&
+                       Mathf.Abs(a.g - b.g) < epsilon &&
+                       Mathf.Abs(a.b - b.b) < epsilon &&
+                       Mathf.Abs(a.a - b.a) < epsilon;
+            }
+
+            [ContextMenu("Clear Grid")]
+            private void ClearGrid()
+            {
+                foreach (Transform child in gridParent)
+                {
+                    if (child.TryGetComponent<GridCell_PatternWeaving>(out GridCell_PatternWeaving cell))
+                    {
+                        cell.SetColor(new Color(1f, 1f, 1f, 0));
+                    }
+                }
+            }
+
+            private IEnumerator ViewPath()
+            {
+                ClearGrid();
+
+                GridCell_PatternWeaving current = selectedPattern.startCell;
+
+                while (current != null && Application.isPlaying)
+                {
+                    current.SetColor(current.data.Find(d => d.texture == selectedPattern.texture).color);
+                    yield return new WaitForSeconds(0.5f);
+                    current = current.data.Find(d => d.texture == selectedPattern.texture).nextCell;
+                }
+            }
+
+            public void PatternFinished()
+            {
+                patterns.Find(p => p.texture == selectedPattern.texture).isFinished = true;
+
+                foreach (Pattern pattern in patterns)
+                {
+                    if (!pattern.isFinished)
+                    {
+                        patternSelectorCanvas.SetActive(true);
+                        ClearGrid();
+                        return;
+                    }
+                }
+
+                EndGame();
+            }
+
+            #region Button Function
+
+            public void BS_SelectMotif(int motif_index)
+            {
+                selectedPattern = patterns[motif_index];
+                patternImg.sprite = Sprite.Create(selectedPattern.texture, new Rect(0, 0, selectedPattern.texture.width, selectedPattern.texture.height), Vector2.zero);
+                selectedPattern.startCell.SetSelectedData(selectedPattern.texture);
+                selectedPattern.startCell.SetIsNextCell();
+
+                patternImg.gameObject.SetActive(true);
+                patternSelectorCanvas.SetActive(false);
+
+                if (!firstPattern)
+                {
+                    StartGame();
+                    firstPattern = true;
+                }
+            }
+
+            #endregion
+
+        }
+    }
+}
