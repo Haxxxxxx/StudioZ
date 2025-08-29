61c1f2f16071df41e6df275de7835f790abdbbb9
Evan Choquet <echoquet@gaming.tech>
2025-08-29 10:40:39 +0200

    UPDATE teacher

---
+++ StudioZCore/Assets/Scripts/Managers/TeacherManager.cs
@@ -0,0 +1,38 @@
+﻿using FishNet;
+using FishNet.Transporting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TeacherManager : MonoBehaviour
+{
+
+    [Header("UI References")]
+    [SerializeField] private GameObject mainMenuCanvas;
+
+
+
+    private void OnEnable()
+    {
+        InstanceFinder.ServerManager.OnServerConnectionState += OnServerStateChange;
+    }
+
+    private void OnDisable()
+    {
+        if(InstanceFinder.ServerManager != null)
+            InstanceFinder.ServerManager.OnServerConnectionState -= OnServerStateChange;
+    }
+
+    private void OnServerStateChange(ServerConnectionStateArgs args)
+    {
+        if (args.ConnectionState == LocalConnectionState.Started)
+        {
+            mainMenuCanvas.SetActive(false);
+            Debug.Log("✅ Serveur lancé !");
+        }
+        else if (args.ConnectionState == LocalConnectionState.Stopped)
+        {
+            mainMenuCanvas.SetActive(true);
+            Debug.Log("❌ Serveur arrêté.");
+        }
+    }
+}
